23696b7a7cc382ef07bc37eec31d87f26c01076e
ewu <es.nj.wu@gmail.com>
2018-10-03 20:46:57 -0700

    added categories to page-3 and linked pages together

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,27 @@
 $(document).ready(function () {
 
+    // $('#next-page').on('click', function () {
+    //     $('#page-1').css('display', "none");
+    //     //this will display the arrow so we can go back
+    //     $('.backarrow').css('visibility', 'visible');
+    //     $('#page-3').css('display', 'block');
+    //     $('.box-1').fadeIn();
+    //     $('.box-2').fadeIn(1000);
+    //     $('.box-3').fadeIn(2000);
+    //     $('.box-4').fadeIn(3000);
+    //     $('.box-5').fadeIn(3000);
+    //     $('.box-6').fadeIn(2000);
+    //     $('.box-7').fadeIn(1000);
+    //     $('.box-8').fadeIn();
+    // })
 
-    $('#next-page').on('click', function () {
-        $('#page-1').css('display', "none");
-        //this will display the arrow so we can go back
-        $('.backarrow').css('visibility', 'visible');
-        $('#page-2').css('display', 'block');
-        $('.box-1').fadeIn();
-        $('.box-2').fadeIn(1000);
-        $('.box-3').fadeIn(2000);
-    })
+    //fading in elements of categories page
+    $('.box-1').fadeIn();
+    $('.box-2').fadeIn(1000);
+    $('.box-3').fadeIn(2000);
+    $('.box-4').fadeIn(3000);
+    $('.box-5').fadeIn(3000);
+    $('.box-6').fadeIn(2000);
+    $('.box-7').fadeIn(1000);
+    $('.box-8').fadeIn();
 })
