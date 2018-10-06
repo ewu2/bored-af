37944f105bf1b1743382315a8907ea23d35a4232
ewu <es.nj.wu@gmail.com>
2018-10-06 11:54:17 -0700

    added indoor/outdoor pages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    //fading in elements of categories page
+    //fading in elements of indoor page
     $('.box-1').fadeIn();
     $('.box-2').fadeIn(1000);
     $('.box-3').fadeIn(2000);
@@ -9,4 +9,12 @@ $(document).ready(function () {
     $('.box-6').fadeIn(2000);
     $('.box-7').fadeIn(1000);
     $('.box-8').fadeIn();
+
+    //fading in elements of outdoor page
+    $('.out-box-1').fadeIn();
+    $('.out-box-2').fadeIn(1000);
+    $('.out-box-3').fadeIn(2000);
+    $('.out-box-4').fadeIn(2000);
+    $('.out-box-5').fadeIn(1000);
+    $('.out-box-6').fadeIn();
 })
